8cd741b90e2dc25e46c258c367ef305216e81ad0
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-20 09:12:24 -0300

    apostila02: fim da secao 2.3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import { createServer } from "@graphql-yoga/node";
 
+const livros = [
+  {
+    id: '1',
+    titulo: 'Effective Java',
+    genero: "Técnico",
+    edicao: 3,
+    preco: 39.99
+  },
+  {
+    id: '2',
+    titulo: 'Concrete Mathematics',
+    genero: 'Tecnico',
+    edicao: 1,
+    preco: 89.99
+  }
+]
+
 //especificação de interface
 const typeDefs = `
   type Livro {
@@ -10,6 +27,10 @@ const typeDefs = `
     preco: Float
   },
   type Query{
+    livros(precoMaximo: Float!): [Livro!]!
+    adicionar (numeros: [Float!]!): Float!
+    notas: [Int!]!
+    bemVindo(nome: String): String!
     effectiveJava: Livro!
   }
 `
@@ -29,6 +50,22 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
+    livros (parent, args, ctx, info){
+      return livros.filter(l => l.preco <= args.precoMaximo)
+    },
+    adicionar(parent, args, ctx, info){
+      return args.numeros.length === 0 ? 0 : args.numeros.reduce((ac, v) => ac + v)
+    },
+    notas (parent, args, ctx, info){
+      return [10, 2, 7, 7, 8]
+    },
+    bemVindo(parent, args, ctx, info){
+      console.log(`parent: ${JSON.stringify(parent)}`)
+      console.log(`args: ${JSON.stringify(args)}`)
+      // console.log(`ctx: ${JSON.stringify(ctx)}`)
+      // console.log(`info: ${JSON.stringify(info)}`)
+      return `Bem vindo, ${args.nome ? args.nome : ' visitante'}`
+    },
     effectiveJava(){
       return {
         id: '123456',
